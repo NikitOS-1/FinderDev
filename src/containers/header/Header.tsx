bee2c7e08860cff779d337e516e5f221c28d0e9c
nikita <NikitaPuzyrenko93@gmail.com>
2024-10-01 03:13:41 +0300

    [EA-3] Add structure for containers

---
+++ src/containers/header/Header.tsx
@@ -0,0 +1,11 @@
+import { HeaderWrapper } from './header.styled.ts';
+
+function Header() {
+  return (
+    <HeaderWrapper>
+      <p>Header</p>
+    </HeaderWrapper>
+  );
+}
+
+export default Header;
