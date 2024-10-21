32b9b9f260897865ca26cf66d2a3e188e9135de7
nikita <NikitaPuzyrenko93@gmail.com>
2024-10-22 00:52:41 +0300

    add user interface and fetch

--- src/commons/types/interfaces.ts
+++ src/commons/types/interfaces.ts
@@ -1,3 +1,37 @@
 export interface UserInterface {
-  id:string
+  login: string,
+  id: number,
+  avatar: string
+  name: string
+  company: string | null,
+  blog: string
+  location: string
+  bio: string | null,
+  twitter: string | null,
+  repos: number
+  followers: number,
+  following: number,
+  created: string
+}
+
+export interface FetchUserInterface {
+  login: string,
+  id: number,
+  avatar_url: string
+  name: string
+  company: string | null,
+  blog: string
+  location: string
+  bio: string | null,
+  public_repos: string
+  twitter_username: string | null,
+  followers: number,
+  following: number,
+  created_at: string
+}
+
+export interface FetchErrorInterface {
+  message: string;
+  documentation_url: string;
+  status: string;
 }
--- src/containers/app/App.tsx
+++ src/containers/app/App.tsx
@@ -2,8 +2,23 @@ import Header from '../header/Header.tsx';
 import Body from '../body/Body.tsx';
 import Footer from '../footer/Footer.tsx';
 import { Container } from './app.styled.ts';
+import { useEffect } from 'react';
 
 function App() {
+  const fetchGitHubData = async (url: string) => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      return console.log(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchGitHubData('https://api.github.com/users/NikitOS-1');
+  }, []);
+
   return (
     <Container>
       <Header />
